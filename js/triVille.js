5df5d41f4aedfd0ae25a550f4436a35814fb6c73
Farouk SAKHER <farouk.sakher@Digital-Grenoble.local>
2021-03-26 16:51:59 +0100

    tri ville avec tri rapide

--- js/triVille.js
+++ js/triVille.js
@@ -61,8 +61,23 @@ function getArrayCsv(csv) {
  * @returns la distance qui sépare la ville de Grenoble
  */
 function distanceFromGrenoble(ville) {
-    console.log('implement me !');
-    return 0;
+    const R = 6371e3; // metres
+    const lat1 = 45.188529;
+    const lon1 = 5.724524;
+    const lat2 = ville.latitude;
+    const lon2 = ville.longitude;
+    const φ1 = lat1 * Math.PI / 180; // φ, λ in radians
+    const φ2 = lat2 * Math.PI / 180;
+    const Δφ = (lat2 - lat1) * Math.PI / 180;
+    const Δλ = (lon2 - lon1) * Math.PI / 180;
+
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const d = R * c; // in metres
+    return d;
 }
 
 /**
@@ -73,8 +88,9 @@ function distanceFromGrenoble(ville) {
  * @return vrai si la ville i est plus proche
  */
 function isLess(i, j) {
-    console.log('implement me !');
-    return true;
+    if (i.distanceFromGrenoble < j.distanceFromGrenoble) {
+        return true;
+    }
 }
 
 /**
@@ -82,20 +98,23 @@ function isLess(i, j) {
  * @param {*} i 
  * @param {*} j 
  */
-function swap(i, j) {
-    console.log('implement me !');
+function swap(tab, i, j) {
+    const temp = tab[i];
+    tab[i] = tab[j];
+    tab[j] = temp;
+    nbPermutation++
 }
 
 function sort(type) {
     switch (type) {
         case 'insert':
-            insertsort();
+            insertsort(listVille);
             break;
         case 'select':
             selectionsort();
             break;
         case 'bubble':
-            bubblesort();
+            bubblesort(listVille);
             break;
         case 'shell':
             shellsort();
@@ -107,21 +126,46 @@ function sort(type) {
             heapsort();
             break;
         case 'quick':
-            quicksort();
+            quicksort(listVille);
             break;
     }
 }
 
-function insertsort() {
-    console.log("insertsort - implement me !");
+function insertsort(tab) {
+    let n = tab.length;
+    for(let i = 1; i < n; i++) {
+        let tmp = tab[i];
+        let j = i - 1;
+        while (j > 0 && tab[j] > tmp) {
+            tab[j+1].distanceFromGrenoble = tab[j].distanceFromGrenoble
+            j--
+        }
+        tab[j+1] = tmp
+    }
+    return tab
+
 }
+
+
 
 function selectionsort() {
     console.log("selectionsort - implement me !");
 }
 
-function bubblesort() {
-    console.log("bubblesort - implement me !");
+function bubblesort(tab) {
+    let swap = true;
+    while (swap) {
+        swap = false;
+        for (let i = 0; i < tab.length - 1; i++) {
+            if (tab[i].distanceFromGrenoble > tab[i + 1].distanceFromGrenoble) {
+                let tmp = tab[i].distanceFromGrenoble;
+                tab[i].distanceFromGrenoble = tab[i + 1].distanceFromGrenoble;
+                tab[i + 1].distanceFromGrenoble = tmp;
+                swap = true;
+            }
+        }
+    }
+
 }
 
 function shellsort() {
@@ -137,8 +181,32 @@ function heapsort() {
     console.log("heapsort - implement me !");
 }
 
-function quicksort() {
-    console.log("quicksort - implement me !");
+function quicksort(tab) {
+    let n = tab.length
+
+    function tri_rapide(tab, premier = 0, dernier = n - 1) {
+        if (premier < dernier) {
+            let pivot = dernier
+            pivot = partitionner(tab, premier, dernier);
+            tri_rapide(tab, premier, pivot - 1);
+            tri_rapide(tab, pivot + 1, dernier)
+        }
+    }
+
+    function partitionner(tab, premier, dernier) {
+        // swap(tab, pivot, dernier);
+        let j = premier;
+        for (let i = premier; i < dernier; i++) {
+            if (tab[i].distanceFromGrenoble <= tab[dernier].distanceFromGrenoble) {
+                swap(tab, i, j);
+                j++;
+            }
+        }
+        swap(tab, dernier, j);
+        return j
+    }
+    tri_rapide(tab);
+
 }
 
 /** MODEL */
